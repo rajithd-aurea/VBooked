7052212a58203e2a4a487d153316591912b1c641
Lorenzo Lino Andres B. Bahinting <lorenz53192@gmail.com>
2016-05-04 02:05:57 +0800

    Implemented refactored code for sending of email conformation to registered host.

--- EventBookingPlatform/EventBookingPlatform.UnitTest/EmailUnitTest/EmailUnitTest.cs
+++ EventBookingPlatform/EventBookingPlatform.UnitTest/EmailUnitTest/EmailUnitTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventBookingPlatform.Helpers;
+
+using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EventBookingPlatform.UnitTest.EmailUnitTest
@@ -7,36 +10,27 @@ namespace EventBookingPlatform.UnitTest.EmailUnitTest
     public class EmailUnitTest
     {
         [TestMethod]
-        public void Email_SendEmailConfirmation_Test()
+        public async Task Email_SendEmailConfirmation_Test()
         {
-            // Arrange
-            bool isEmailSent = false;
-            string host = "smtp.gmail.com";
-            int port = 25;
-            string user = "lorenz53192@gmail.com";
-            string pass = "email53192";
-            string sender = "lorenz53192@gmail.com";
-            string title = "VBooked Registration Confirmation";
-            string recipient = "developer.lorenz@gmail.com";
-
-
-            // Act
             try
             {
-                System.Net.Mail.MailMessage m = new System.Net.Mail.MailMessage();
-                m.From = new System.Net.Mail.MailAddress(sender, title);
-                m.To.Add(new System.Net.Mail.MailAddress(recipient));
-                //m.Body = string.Format("Dear {0} <BR /> Thank you for your registration, please click on the below link to complete your registration: <a href =\"{1}\" title =\"User Email Confirm\">{1}</a>", user.UserName, Url.Action("ConfirmEmail", "Account", new { Token = user.Id, Email = user.Email }, Request.Url.Scheme)) ;
-                m.Body = "Test email";
-                m.Subject = "Test - VBooked Registration confirmation";
-                m.IsBodyHtml = true;
+                // Arrange
+                bool isEmailSent = false;
+                EmailHelper emailHelper = new EmailHelper
+                {
+                    Host = "mail.vbooked.com",
+                    Sender = "support@vbooked.com",
+                    Recipient = "developer.lorenz@gmail.com",
+                    Subject = "VBooked Registration Confirmation",
+                    NetworkUser = "support@vbooked.com",
+                    NetworkPass = "supportmail123!",
+                    UserEmail = "developer.lorenz@gmail.com",
+                    UserPassword = "Demo123!",
+                    ConfirmationUrl = "http://www.vbooked.com/ConfirmEmail"
+                };
 
-                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient(host, port);
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new System.Net.NetworkCredential(user, pass);
-                smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-                smtp.EnableSsl = true;
-                smtp.Send(m);
+                // Act
+                await emailHelper.SendEmail();
 
                 isEmailSent = true;
 
@@ -44,7 +38,7 @@ namespace EventBookingPlatform.UnitTest.EmailUnitTest
             }
             catch (Exception ex)
             {
-                Assert.Fail(ex.Message);
+                Assert.Fail(ex.InnerException.ToString());
             }
         }
     }
