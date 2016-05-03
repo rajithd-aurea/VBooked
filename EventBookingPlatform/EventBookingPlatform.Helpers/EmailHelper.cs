6242299366cd1e1475a88d63b986809d6d4ad7c6
Lorenzo Lino Andres B. Bahinting <lorenz53192@gmail.com>
2016-05-04 01:59:12 +0800

    Made a unit test for sending of registration email confirmation to host that registered.

--- EventBookingPlatform/EventBookingPlatform.Helpers/EmailHelper.cs
+++ EventBookingPlatform/EventBookingPlatform.Helpers/EmailHelper.cs
@@ -1,66 +1,72 @@
 ﻿using System.Net.Mail;
 using System.Net;
 using System.Text;
+using System;
+using System.Threading.Tasks;
 
 namespace EventBookingPlatform.Helpers
 {
     public class EmailHelper
     {
-        public string host { get; set; }
-        public int port { get; set; }
-        public string networkUser { get; set; }
-        public string networkPass { get; set; }
-        public string sender { get; set; }
-        public string title { get; set; }
-        public string recipient { get; set; }
-        public string acctUsername { get; set; }
-        public string acctPassword { get; set; }
-        public string confirmationUrl { get; set; }
+        public string Host { get; set; }
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string NetworkUser { get; set; }
+        public string NetworkPass { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public string ConfirmationUrl { get; set; }
 
         public EmailHelper()
         {
         }
 
-        public EmailHelper(string host, int port, string networkuser, string networkpass, string sender, string title, string recipient, string acctusername, string acctpass, string confirmationurl)
+        public EmailHelper(string host, string sender, string recipient, string subject, string networkuser, string networkpass, string useremail, string userpassword, string confirmationurl)
         {
-            this.host = host;
-            this.port = port;
-            this.networkUser = networkuser;
-            this.networkPass = networkpass;
-            this.sender = sender;
-            this.title = title;
-            this.recipient = recipient;
-            this.acctUsername = acctusername;
-            this.acctPassword = acctPassword;
-            this.confirmationUrl = confirmationurl;
+            Host = host;
+            NetworkUser = networkuser;
+            NetworkPass = networkpass;
+            Sender = sender;
+            Recipient = recipient;
+            UserEmail = useremail;
+            UserPassword = userpassword;
+            ConfirmationUrl = confirmationurl;
         }
 
-        public void SendEmail()
+        public async Task SendEmail()
         {
-            MailMessage message = new MailMessage();
-            message.From = new MailAddress(this.sender, this.title);
-            message.To.Add(new MailAddress(this.confirmationUrl));
-            message.Body = EmailContent();
-            message.Subject = "VBooked Registration Confirmation";
-            message.IsBodyHtml = true;
+            try
+            {
+                MailMessage message = new MailMessage(Sender, Recipient);
+                message.BodyEncoding = Encoding.Default;
+                message.Subject = Subject;
+                message.Body = EmailContent();
+                message.Priority = MailPriority.High;
+                message.IsBodyHtml = true;
 
-            SmtpClient smtp = new SmtpClient(this.host, this.port);
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential(this.networkUser, this.networkPass);
-            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtp.EnableSsl = true;
-            smtp.Send(message);
+                SmtpClient smtp = new SmtpClient(Host);
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = false;
+                smtp.Credentials = new NetworkCredential(NetworkUser, NetworkPass);
+                await smtp.SendMailAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.InnerException);
+            }
         }
 
         private string EmailContent()
         {
             StringBuilder content = new StringBuilder();
-            content.Append("Dear " + this.recipient + "<br />");
-            content.Append("Thank you for registering at http://wwww.vbooked.com! Below is your login details. Before you can login, please confirm your email by clicking at " + "<a href=" + this.confirmationUrl + ">THIS LINK" + "</a>" + "<br />");
+            content.Append("Dear " + Recipient + "<br />");
+            content.Append("Thank you for registering at http://wwww.vbooked.com! Below is your login details. Before you can login, please confirm your email by clicking at " + "<a href=" + ConfirmationUrl + ">Confirm Registration" + "</a>" + "<br />");
             content.Append("<br />");
             content.Append("Login Credentials" + "<br />");
-            content.Append("Email: " + this.acctUsername + "<br />");
-            content.Append("Password: " + this.acctPassword);
+            content.Append("Email: " + UserEmail + "<br />");
+            content.Append("Password: " + UserPassword);
 
             return content.ToString();
         }
