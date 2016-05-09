31a31d5fcb5fcdd4c3aba3b9bfef670580efe74c
Lorenzo Lino Andres B. Bahinting <lorenz53192@gmail.com>
2016-05-09 23:26:22 +0800

    Added VenueController.cs.

---
+++ EventBookingPlatform/EventBookingPlatform/Controllers/VenueController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace EventBookingPlatform.Controllers
+{
+    [Authorize(Roles = "Admin, Host")]
+    public class VenueController : Controller
+    {
+        [HttpGet]
+        public ActionResult Dashboard(string hostid)
+        {
+            return View();
+        }
+    }
+}
