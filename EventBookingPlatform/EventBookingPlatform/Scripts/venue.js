2c2106b7b220079adc8ed1273bc4056307cbf807
Lanz Bahinting <Lanz Bahinting>
2016-05-21 19:19:27 +0800

    Added form validation to AddPlace.cshtml.

--- EventBookingPlatform/EventBookingPlatform/Scripts/venue.js
+++ EventBookingPlatform/EventBookingPlatform/Scripts/venue.js
@@ -23,4 +23,40 @@
 
 $(document).ready(function () {
     venue.getVenues();
+
+    $('#frmAddPlace').validate({
+        rules: {
+            venuename: "required",
+            venuetype: "required",
+            foundationyear: "required",
+            regno: "required",
+            renovationyear: "required",
+            area: "required",
+            openspaces: "required",
+            service: "required",
+            seasonactivity: "required",
+            location: "required",
+            settlement: "required",
+            street: "required",
+            targeting: "required"
+        },
+        messages: {
+            venuename: "Please enter Venue Name",
+            venuetype: "Please select Venue Type",
+            foundationyear: "Please enter Foundation Year",
+            regno: "Please enter Registration #",
+            renovationyear: "Please enter Renovation Year",
+            area: "Please enter Area (sq)",
+            openspaces: "Please enter Open Spaces",
+            service: "Please select Service",
+            seasonactivity: "Please select Season Activity",
+            location: "Please enter Location",
+            settlement: "Please enter Settlement",
+            street: "Please enter Street",
+            targeting: "Please enter Targeting"
+        },
+        submitHandler: function (form) {
+            alert("Success!");
+        }
+    });
 });
