60e9e2a02ce44832829d80a0e9790b4ea86c2a4c
lorenz31 <developer.lorenz@gmail.com>
2016-05-26 20:55:13 +0800

    Added CountryBLL.cs file.

---
+++ EventBookingPlatform/EventBookingPlatform.BusinessLayer/CountryBLL.cs
@@ -0,0 +1,15 @@
+﻿using EventBookingPlatform.DAL.Entities;
+using EventBookingPlatform.DAL.Repository;
+
+namespace EventBookingPlatform.BusinessLayer
+{
+    public class CountryBLL
+    {
+        private IEFRepository _repo;
+
+        public CountryBLL()
+        {
+            _repo = new Repository();
+        }
+    }
+}
