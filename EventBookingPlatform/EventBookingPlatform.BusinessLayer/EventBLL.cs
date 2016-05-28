220d3279867115524e9e6463e83167abaffa3048
lorenz31 <developer.lorenz@gmail.com>
2016-05-28 11:48:05 +0800

    Added EventBLL.cs file.

---
+++ EventBookingPlatform/EventBookingPlatform.BusinessLayer/EventBLL.cs
@@ -0,0 +1,22 @@
+﻿using EventBookingPlatform.DAL.Entities;
+using EventBookingPlatform.DAL.Repository;
+
+using System.Collections.Generic;
+
+namespace EventBookingPlatform.BusinessLayer
+{
+    public class EventBLL
+    {
+        private IEFRepository _repo;
+
+        public EventBLL()
+        {
+            _repo = new Repository();
+        }
+
+        public IEnumerable<PrivateEvent> GetPrivateEvents()
+        {
+            return _repo.GetPrivateEvents();
+        }
+    }
+}
