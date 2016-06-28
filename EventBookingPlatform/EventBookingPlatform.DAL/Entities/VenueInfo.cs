959e66911aa5bd1e61cd4a5500b0de967a73848b
lorenz31 <developer.lorenz@gmail.com>
2016-06-28 21:08:33 +0800

    Updated Ado.Net EDM file. Updated VenueInfo table.

--- EventBookingPlatform/EventBookingPlatform.DAL/Entities/VenueInfo.cs
+++ EventBookingPlatform/EventBookingPlatform.DAL/Entities/VenueInfo.cs
@@ -48,8 +48,10 @@ namespace EventBookingPlatform.DAL.Entities
         public string SeasonActs { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
-        public string Street { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
         public string ZipCode { get; set; }
+        public string PBox { get; set; }
     
         public virtual AspNetUser AspNetUser { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
